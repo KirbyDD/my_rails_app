5b880901c143d614bed8831df63b5c11a4b1f8f8
DeMarcus Kirby <tkd024@gmail.com>
2020-01-16 14:48:01 -0700

    Add session controller to facilitate logging in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      error = Error.new
+      flash[:notice] = error.bad_credentials
+      redirect_to "/login"
+    end
+  end
+
+end
