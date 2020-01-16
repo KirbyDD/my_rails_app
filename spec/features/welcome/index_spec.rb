d563182349f298a61273f05ba839c4ea42338ad2
DeMarcus Kirby <tkd024@gmail.com>
2020-01-16 14:42:49 -0700

    Update test to expect additional content

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe "As a visitor", type: :feature do
   it 'Should say hello to visitor' do
     visit '/'
     expect(page).to have_content('Welcome!')
+    expect(page).to have_content('Register')
+    expect(page).to have_content('Login')
   end
 end
