26577b7acc3086398de5e01f5d1b78dda29d7e29
DeMarcus Kirby <tkd024@gmail.com>
2020-03-10 23:44:17 -0600

    Add joins table relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest
+
+  has_many :user_animes
+  has_many :animes, through: :user_animes
   has_secure_password
 end
